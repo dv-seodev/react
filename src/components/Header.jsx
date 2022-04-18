c355483fa3e66ea7beec7a7bb766d3deba42617b
dv-seodev <dv.volynkin@gmail.com>
2022-04-18 21:38:59 +0300

    project added

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className='header'>
+            <img src="https://assets.coingecko.com/coins/images/119/large/Syscoin.png"></img>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+function Profile() {
+    return (
+        <div className='content'>
+            <img src="https://s1.1zoom.me/big3/81/Russia_Mountains_Lake_469922.jpg"></img>
+
+            <div>
+                ava + description
+            </div>
+            <div>
+                my posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
